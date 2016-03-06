d8500499a476c0d34654cd5459516e2adace632f
utleroy <bryanhankins@me.com>
2016-03-05 23:07:47 -0600

    added js and css links in html page and added object arrays in js

---
+++ wysiwyg.js
@@ -0,0 +1,40 @@
+var famous1 = {
+	title: "Musician",
+	name: "Elvis Presley",
+    bio: "lived sang died"
+    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
+    lifespan: {
+    	birth: "1935",
+    	death: "1977"
+    }
+}
+var famous2 = {
+	title: "Musician",
+	name: "Elvis Presley",
+    bio: "lived sang died"
+    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
+    lifespan: {
+    	birth: "1935",
+    	death: "1977"
+    }
+}
+var famous3 = {
+	title: "Musician",
+	name: "Elvis Presley",
+    bio: "lived sang died"
+    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
+    lifespan: {
+    	birth: "1935",
+    	death: "1977"
+    }
+}
+var famous4 = {
+	title: "Musician",
+	name: "Elvis Presley",
+    bio: "lived sang died"
+    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
+    lifespan: {
+    	birth: "1935",
+    	death: "1977"
+    }
+}
