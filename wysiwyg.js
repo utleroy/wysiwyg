6e43ce007ab71d7a9f2ac7fb414aac98ba03cef9
utleroy <bryanhankins@me.com>
2016-03-07 17:47:35 -0600

    modified js array

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,4 +1,32 @@
-var famous1 = {
+var famousPeople = [
+   {
+	title: "Musician",
+	name: "Elvis Presley",
+    bio: "lived sang died"
+    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
+    lifespan: {
+    	birth: "1935",
+    	death: "1977"
+    },
+   {
+	title: "Musician",
+	name: "Elvis Presley",
+    bio: "lived sang died"
+    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
+    lifespan: {
+    	birth: "1935",
+    	death: "1977"
+    },
+   {
+	title: "Musician",
+	name: "Elvis Presley",
+    bio: "lived sang died"
+    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
+    lifespan: {
+    	birth: "1935",
+    	death: "1977"
+    },
+   {
 	title: "Musician",
 	name: "Elvis Presley",
     bio: "lived sang died"
@@ -7,36 +35,9 @@ var famous1 = {
     	birth: "1935",
     	death: "1977"
     }
-}
-var famous2 = {
-	title: "Musician",
-	name: "Elvis Presley",
-    bio: "lived sang died"
-    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
-    lifespan: {
-    	birth: "1935",
-    	death: "1977"
-    }
-}
-var famous3 = {
-	title: "Musician",
-	name: "Elvis Presley",
-    bio: "lived sang died"
-    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
-    lifespan: {
-    	birth: "1935",
-    	death: "1977"
-    }
-}
-var famous4 = {
-	title: "Musician",
-	name: "Elvis Presley",
-    bio: "lived sang died"
-    image: "http://images3.mtv.com/uri/mgid:file:docroot:cmt.com:/sitewide/assets/img/artists/presley_elvis/elvispresley02-x600.jpg?enlarge=false&matte=true&matteColor=black&quality=0.85"
-    lifespan: {
-    	birth: "1935",
-    	death: "1977"
-    }
+  ]
+
+
 
     /*Create an array of objects that represents famous people (see structure below).
 Create a text input in your DOM.
